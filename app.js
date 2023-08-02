3fd971ffd4b9751d9b0941b003d9a7e8bbb4bcb7
michel <msampil@switch.com.uy>
2023-08-01 21:50:31 -0300

    basic structure and readme ex

---
+++ app.js
@@ -0,0 +1,28 @@
+// 0 - rickRollingUser() una funcion
+// que dado un tiempo redirija al
+// usuario a: 
+// https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+// 1 -  Cambiar el titulo de la pagina por
+// "80' game 🎮 ", "insert 🪙 " 
+
+// 2 -  Cambiar el titulo de la pagina por
+// "80' game 🎮 ", "insert 🪙 "  y "🥔 You won!" 
+
+// 3 - annoyingMessage() hacer una funcion que
+// cada 5 segundos aparezca un alert con el mensaje
+// "Get a Premium Account 🤩" 
+
+// 4 - Hacer intermitente el mensaje de "Please, insert coin 🪙..."
+
+// 5 - annoyingMessage() hacer una funcion que
+// cada un segundo aparezca un alert con el mensaje
+// "🎉 you won $1.000.000 💰" 
+
+// 6 - Implementar la funcion fetchScores()  
+// traer los datos de la tabla de posiciones
+// utilizando fetch(), puede ser desde un json
+// server o de alguna fake api
+
+// 7 - Actualizar constantemente el scoreboard
+//  de la pagina. Utilizando polling.
