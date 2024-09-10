29ac1c13d8571446f07aec43a5b62488e027db6a
michel sampil <michel.sampil@gmail.com>
2024-09-10 15:59:22 -0300

    adding music button, some styles refactor

--- app.js
+++ app.js
@@ -24,18 +24,39 @@ function onClickCharacter(event) {
 }
 
 addCharacterHandlers();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const musicControlButton = document.getElementById("music-control");
+  const audio = document.getElementById("background-music");
+
+  let isPlaying = false;
+
+  musicControlButton.addEventListener("click", function () {
+    if (isPlaying) {
+      audio.pause();
+      musicControlButton.textContent = "🎶";
+    } else {
+      audio.play();
+      musicControlButton.textContent = "🔇";
+    }
+    isPlaying = !isPlaying;
+  });
+});
 // END FOR FUN CODE
 
+// TODO, El ejercicio empieza aca 👇
+// Note: las funciones 0,1,2 hay que comentar la llamada para evitar que molesten al resto del ejercicio.
+
 // 0 - rickRollingUser() una funcion
-// que dado un tiempo redirija al
+// que dado un tiempo por ejemplo 30 segundos redirija al
 // usuario a:
 // https://www.youtube.com/watch?v=dQw4w9WgXcQ
 
-// 2 -  Cambiar el titulo de la pagina por
+// 1 -  Cambiar el titulo de la pagina por cada un segundo.
 // "80' game 🎮 ", "insert 🪙 "  y "🥔 You won!"
 
-// 3 - annoyingMessage() hacer una funcion que
-// cada 5 segundos aparezca un alert con el mensaje
+// 2 - annoyingMessage() hacer una funcion que
+// cada 1 minuto aparezca un alert con el mensaje  "Get a Premium Account 🤩"
 
 // 4 - Hacer intermitente el mensaje de "Please, insert coin 🪙..."
 // tip: utilizar las clases de css fadeIn fadeOut
@@ -43,12 +64,11 @@ addCharacterHandlers();
 // 5 - Implementar la funcion fetchScores()
 // traer los datos de la tabla de posiciones
 // utilizando fetch(), puede ser desde un json
-// server o de alguna fake api. se provee db.json de ejemplo
-// let scores = [];
+// server o de alguna fake api como mocki.io.
+// se provee de un db.json de ejemplo
 
 // 6 - Actualizar constantemente el scoreboard
 // de la pagina utilizando polling.
-// FOR FUN SELECTED CHARACTER CODE
 
 function addCharacterHandlers() {
   const characterElements = [
@@ -67,163 +87,3 @@ function onClickCharacter(event) {
   const audio = new Audio(clickedCharacterSound);
   audio.play();
 }
-
-addCharacterHandlers();
-// END FOR FUN CODE
-
-// 0 - rickRollingUser() una funcion
-// que dado un tiempo redirija al
-// usuario a:
-// https://www.youtube.com/watch?v=dQw4w9WgXcQ
-function rickRollingUser() {
-  setTimeout(() => {
-    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  }, 300000);
-}
-
-rickRollingUser();
-
-// 1 -  Cambiar el titulo de la pagina por
-// "80' game 🎮 ", "insert 🪙 "
-// let isFirstTitleTurn = true;
-
-// function alternateTitle() {
-//   const firstTitle = "80' game 🎮 ";
-//   const secondTitle = "Insert a 🪙 ";
-//   let currentTitle;
-
-//   if (isFirstTitleTurn) {
-//     currentTitle = firstTitle;
-//   } else {
-//     currentTitle = secondTitle;
-//   }
-//   isFirstTitleTurn = !isFirstTitleTurn;
-//   document.title = currentTitle;
-// }
-
-// function titleTimer() {
-//   setInterval(alternateTitle, 1000);
-// }
-
-// titleTimer();
-
-// 2 -  Cambiar el titulo de la pagina por
-// "80' game 🎮 ", "insert 🪙 "  y "🥔 You won!"
-let iterator = 0;
-
-function changeTitle() {
-  const titleCollection = ["80' game 🎮 ", "insert 🪙 ", "🥔 You won!"];
-  const titleIndex = iterator % titleCollection.length;
-  const newTitle = titleCollection[titleIndex];
-  document.title = newTitle;
-  iterator++;
-}
-
-function alternateTitle() {
-  setInterval(changeTitle, 1000);
-}
-
-alternateTitle();
-
-// 3 - annoyingMessage() hacer una funcion que
-// cada 5 segundos aparezca un alert con el mensaje
-// "Get a Premium Account 🤩"
-function annoyingMessage() {
-  setInterval(() => {
-    window.alert("Get a Premium Account 🤩");
-  }, 5000);
-}
-
-// annoyingMessage();
-
-// 4 - Hacer intermitente el mensaje de "Please, insert coin 🪙..."
-// tip: utilizar las clases de css fadeIn fadeOut
-function fadeIn() {
-  const waitingBoardElem = document.getElementsByClassName("waiting-board")[0];
-  waitingBoardElem.setAttribute("class", "waiting-board up");
-}
-
-function fadeOut() {
-  const waitingBoardElem = document.getElementsByClassName("waiting-board")[0];
-  waitingBoardElem.setAttribute("class", "waiting-board down");
-}
-
-let isFadeIn = true;
-
-function fadingMessage() {
-  console.log("aca");
-  setInterval(() => {
-    if (isFadeIn) {
-      fadeIn();
-    } else {
-      fadeOut();
-    }
-    isFadeIn = !isFadeIn;
-  }, 1000);
-}
-
-fadingMessage();
-
-// 5 - Implementar la funcion fetchScores()
-// traer los datos de la tabla de posiciones
-// utilizando fetch(), puede ser desde un json
-// server o de alguna fake api. se provee db.json de ejemplo
-let scores = [];
-
-const url = "http://localhost:3000/scores";
-
-async function fetchScores() {
-  try {
-    const response = await fetch(url, { method: "GET" });
-    const data = await response.json();
-
-    if (JSON.stringify(data) === JSON.stringify(scores)) {
-      deleteOldScores();
-    }
-
-    scores = data;
-    console.log("scores in fetch: ", scores);
-  } catch (error) {
-    console.log("Error fetching data: ", error);
-  }
-}
-
-function showScores() {
-  const list = document.getElementsByTagName("ul")[0];
-  let positionNumber = 1;
-  scores.forEach((e) => {
-    const newItem = document.createElement("li");
-    const textNode = document.createTextNode(
-      `Position: ${positionNumber} Score: ${e.score}, Name: ${e.name}, Character: ${e.character}.`
-    );
-    newItem.appendChild(textNode);
-    list.appendChild(newItem);
-    positionNumber++;
-  });
-}
-
-async function fetchAndShowScores() {
-  await fetchScores();
-  showScores();
-}
-
-fetchAndShowScores();
-
-// 6 - Actualizar constantemente el scoreboard
-// de la pagina utilizando polling.
-function deleteOldScores() {
-  scores = [];
-  const scoreElemets = [...document.getElementsByTagName("li")];
-  console.log("scoreElments: ", scoreElemets);
-  scoreElemets.forEach((e) => {
-    e.remove();
-  });
-}
-
-async function pollingFetch() {
-  setInterval(() => {
-    fetchAndShowScores();
-  }, 1000);
-}
-
-pollingFetch();
